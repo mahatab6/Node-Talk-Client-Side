4db8c919b7a7269fd0c2ae3c28937400cd25d7c6
mahatab6 <mahatabcall56@gmail.com>
2025-07-08 10:35:33 +0600

    post sort method added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import Banner from './homecomponents/Banner';
 import TagsSection from './homecomponents/left side/TagsSection';
 import Post from './homecomponents/right side/Post';
+import SortPosts from './homecomponents/left side/SortPosts';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
-            <div>
-                
+            <div className='grid lg:grid-cols-11 gap-5 w-11/12 mx-auto py-10'>
+
                 {/* left-side */}
-                <div>
+                <div className='lg:col-span-3 space-y-5'>
                     <TagsSection/>
+                    <SortPosts/>
                 </div>
 
                 {/* right side */}
-                <div>
+                <div className='lg:col-span-8'>
                     <Post/>
                 </div>
             </div>
---
+++ src/pages/home/homecomponents/left side/SortPosts.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { IoMdTime, IoMdTrendingUp } from "react-icons/io";
+import { TbChartBarPopular } from "react-icons/tb";
+
+
+
+const SortPosts = () => {
+    return (
+        <div className=' bg-amber-300 p-4 rounded-2xl justify-items-center flex-col' >
+            <h2 className='flex items-center text-2xl gap-1 justify-center pb-2 font-bold'><IoMdTrendingUp />Sort Posts</h2>
+            <button className='text-xl flex items-center gap-1'><TbChartBarPopular /> Most Popular</button>
+            <button className='text-xl flex items-center gap-1'><IoMdTime /> Most Recent</button>
+        </div>
+    );
+};
+
+export default SortPosts;
