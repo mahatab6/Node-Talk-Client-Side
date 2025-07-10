ec77457a98f6a77328faf19a3e2fc8e725f4d61d
mahatab6 <mahatabcall56@gmail.com>
2025-07-10 14:51:32 +0600

    table data show

--- src/pages/userDashboard/MyPost.jsx
+++ src/pages/userDashboard/MyPost.jsx
@@ -3,20 +3,34 @@ import DashboardText from '../../components/DashboardText';
 import { FaRegComments } from "react-icons/fa";
 import { Link } from 'react-router';
 import { MdDelete } from "react-icons/md";
+import { useQuery } from '@tanstack/react-query';
+import useAxiosToken from '../../hooks/useAxiosToken';
 
 
 
 const MyPost = () => {
 
-    const {data} = useQuery
+    const axiosSecureJWT = useAxiosToken();
 
+    const {data, isLoading} = useQuery({
+        queryKey: ['user-post'],
+        queryFn: async () => {
+            const res = await axiosSecureJWT.get('/specific-post');
+            return res.data;
+        }
+    })
+
+    if(isLoading){
+        return <p>Loading.........</p>
+    }
+    console.log(data)
 
     return (
         <div className='px-6 '>
             <DashboardText/>
 
             <div className='p-10 bg-[#202338] rounded-2xl mb-10'>
-                <h2 className='text-3xl font-bold mb-6'>My Posts (1)</h2>
+                <h2 className='text-3xl font-bold mb-6'>My Posts {data.length}</h2>
                 <div>
                     <div className="overflow-x-auto">
                         <table className="table">
@@ -31,17 +45,32 @@ const MyPost = () => {
                                 </tr>
                             </thead>
 
-                            <tbody>
-                            
-                                <tr className="hover:bg-white/5 font-bold">
-                                    <th>1</th>
-                                    <td><Link className='hover:text-blue-400'>15 life changing lessons from</Link></td>
-                                    <td>0</td>
-                                    <td><Link to=''><FaRegComments className='text-[#4ADE80]' size={30}/></Link></td>
-                                    <td><Link to='/' ><MdDelete className='text-[#C084FC]' size={30}/></Link></td>
-                                </tr>
-
-                            </tbody>
+                            {
+                                data.map((post, index) => (
+                                    <tbody key={post._id}>
+                                        <tr className="hover:bg-white/5 font-bold">
+                                            <th>{index + 1}</th>
+                                            <td>
+                                                <Link className='hover:text-blue-400'>
+                                                    {post.PostTitle}
+                                                </Link>
+                                            </td>
+                                            <td>{post.upVote}</td>
+                                            <td>
+                                                <Link to=''>
+                                                    <FaRegComments className='text-[#4ADE80]' size={30}/>
+                                                </Link>
+                                            </td>
+                                            <td>
+                                                <Link to='/'>
+                                                    <MdDelete className='text-[#C084FC]' size={30}/>
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                ))
+                            }
+                        
                         </table>
                     </div>
                 </div>
