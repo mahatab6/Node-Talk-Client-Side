6ebcdb1834b395354e998fd7292f8177fa19a392
mahatab6 <mahatabcall56@gmail.com>
2025-07-11 00:25:22 +0600

    user active action added

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -5,13 +5,16 @@ import { FaAngleDown, FaAngleUp, FaArrowLeft, FaRegComments } from 'react-icons/
 import { Link, useParams } from 'react-router';
 import useAxiosSecure from '../hooks/useAxiosSecure';
 import { Tooltip } from 'react-tooltip';
+import { FacebookIcon, FacebookShareButton, LinkedinShareButton, LinkedinIcon, TwitterShareButton, TwitterIcon, EmailShareButton, EmailIcon } from 'react-share';
+import useAuth from '../hooks/useAuth';
+
 
 const PostDetails = () => {
-
+    const {user} = useAuth();
     const axiosSecure = useAxiosSecure();
     const {id} = useParams();
     const { register, handleSubmit } = useForm();
-
+    
     
 
     const{ data: postData } = useQuery({
@@ -35,25 +38,42 @@ const PostDetails = () => {
             <div className='py-10 w-9/12 mx-auto'>
             <Link to='/' className='flex items-center text-white gap-2 hover:text-blue-400 text-xl pb-10'><FaArrowLeft />Back to discussions</Link>
 
-            <div className='flex gap-3 bg-[#202237] text-white p-5 rounded-2xl mb-10'>
-                {/* Vote section */}
-                <div className='text-center space-y-1'>
-                    <p data-tooltip-id="my-tooltip" data-tooltip-content="UpVote" className='btn rounded-full p-2 hover:bg-green-500'><FaAngleUp  size={25} /></p>
-                    <p className=' font-bold text-xl'>25</p>
-                    <p data-tooltip-id="my-tooltip" data-tooltip-content="DownVote" className='btn rounded-full p-2 hover:bg-red-500'><FaAngleDown size={25} /></p>
-                    <Tooltip id='my-tooltip'/>
-                </div>
+            <div className=' gap-3 bg-[#202237] text-white p-5 rounded-2xl mb-10'>
+                
     
                 <div className='flex-col space-y-2'>
                     {/* author info */}
-                    <div className='flex items-center space-x-3'>
-                        <div className="avatar avatar-placeholder">
+                    <div className='flex justify-between '>
+                        <div className='flex items-center space-x-3'>
+                            <div className="avatar avatar-placeholder">
                             <div className="bg-neutral text-neutral-content w-12 rounded-full">
                                 <img src={postData?.AuthorImage} alt="" />
                             </div>
                         </div>
                         <h2 className='text-xl'>{postData?.AuthorName}</h2>
                         <p>5h ago</p>
+                        </div>
+
+                        {/* Vote section */}
+                        {
+                            (user)? (
+                                <div className='text-center space-y-1'>
+                                    <p data-tooltip-id="my-tooltip" data-tooltip-content="UpVote" className='btn rounded-full p-2 hover:bg-green-500'><FaAngleUp  size={25} /></p>
+                                    <p className=' font-bold text-xl'>25</p>
+                                    <p data-tooltip-id="my-tooltip" data-tooltip-content="DownVote" className='btn rounded-full p-2 hover:bg-red-500'><FaAngleDown size={25} /></p>
+                                    <Tooltip id='my-tooltip'/>
+                                </div>
+                            ) : (
+                               <div className='text-center space-y-1'>
+                                    <p data-tooltip-id="my-tooltip" data-tooltip-content="login again" disabled className='btn rounded-full p-2 hover:bg-green-500'><FaAngleUp  size={25} /></p>
+                                    <p className=' font-bold text-xl'>25</p>
+                                    <p data-tooltip-id="my-tooltip" data-tooltip-content="login again" disabled className='btn rounded-full p-2 hover:bg-red-500'><FaAngleDown size={25} /></p>
+                                    <Tooltip id='my-tooltip'/>
+                                </div>
+                            )
+                        }
+                        
+    
                     </div>
     
                     {/* author-post-heading */}
@@ -68,23 +88,49 @@ const PostDetails = () => {
                                 {tag.value}
                                 </span>
                             ))}
-                        </div>
-    
+                    </div>
+
+                    <p className='flex items-center gap-2 '><FaRegComments /> 8 comments</p>
     
                     {/* total vote and comment section */}
-                    <div className='flex space-x-2'>
-                        <p className='flex items-center gap-2 '><FaRegComments /> 8 comments</p>
-                        <button className='btn bg-green-600'>Facebook</button>
-                        <button className='btn bg-green-600'>Facebook</button>
-                        <button className='btn bg-green-600'>Facebook</button>
-                    </div>
+                    {
+                        (user) ? (
+                            <div className='flex space-x-2 pt-2'>
+                                <FacebookShareButton  url={postData?._id} quote={postData?.PostTitle}>
+                                    <FacebookIcon size={32} round />
+                                </FacebookShareButton>
+                                <LinkedinShareButton url={postData?._id} quote={postData?.PostTitle}>
+                                    <LinkedinIcon size={32} round />
+                                </LinkedinShareButton>
+                                <TwitterShareButton url={postData?._id} quote={postData?.PostTitle}>
+                                    <TwitterIcon size={32} round />
+                                </TwitterShareButton>
+                                
+                            </div>
+                        ) : (
+                            <div className='flex space-x-2 pt-2'>
+                                <FacebookShareButton disabled cursor-not-allowed url={postData?._id} quote={postData?.PostTitle}>
+                                    <FacebookIcon  size={32} round />
+                                </FacebookShareButton>
+                                <LinkedinShareButton disabled cursor-not-allowed url={postData?._id} quote={postData?.PostTitle}>
+                                    <LinkedinIcon size={32} round />
+                                </LinkedinShareButton>
+                                <TwitterShareButton disabled cursor-not-allowed url={postData?._id} quote={postData?.PostTitle}>
+                                    <TwitterIcon size={32} round />
+                                </TwitterShareButton>
+                                
+                            </div>
+                        )
+                    }
+
                 </div>
+                
             </div>
 
             {/* Comments section */}
             <div className='bg-[#202237] text-white p-5 rounded-2xl space-y-3 '>
                 <h2 className='text-2xl font-bold'>Comments (0)</h2>
-                <div className='flex gap-5'>
+                <div className='flex flex-col md:flex-row gap-5'>
                    <div >
                         <div className=" avatar avatar-placeholder">
                             <div className="bg-neutral text-neutral-content w-14 rounded-full">
@@ -96,7 +142,13 @@ const PostDetails = () => {
                     <div className=''>
                         <form onSubmit={handleSubmit(onSubmit)}>
                             <textarea {...register("comment")} className=' border w-full md:w-100 lg:w-xl rounded-2xl block py-5 p-1' placeholder='Share your thoughts...' />
-                            <button type='submit' className='bg-green-500 p-2 rounded-2xl hover:cursor-pointer mt-3'>Post Comment</button>
+                            {
+                                (user)?(
+                                    <button type='submit' className='bg-green-500 p-2 rounded-2xl hover:cursor-pointer mt-3'>Post Comment</button>
+                                ): (
+                                   <button disabled type='submit' className='bg-green-500 p-2 rounded-2xl mt-3 cursor-not-allowed opacity-50'> Post Comment </button>
+                                )
+                            }
                         </form>
                     </div>
                 </div>
