0d5266587d77fcbeb47b56a976b828ca1ba6447b
mahatab6 <mahatabcall56@gmail.com>
2025-07-19 18:02:31 +0600

    banner bg-image and text change

--- src/pages/home/homecomponents/Banner.jsx
+++ src/pages/home/homecomponents/Banner.jsx
@@ -51,11 +51,11 @@ const Banner = ({ setSearch }) => {
 
     return (
         <div>
-            <div className="bg-[url('assets/banner.jpg')] h-80 bg-cover bg-center">
-                <div className='md:flex items-center justify-center md:py-10'>
+            <div className="bg-[url('assets/banner.jpg')] h-108 bg-cover bg-center">
+                <div className='md:flex items-center justify-center justify-items-center md:py-10'>
                     <div className='space-y-4 text-center p-3 relative w-full max-w-xl'>
-                        <h1 className='text-3xl lg:text-5xl font-bold'>Welcome to Our NodeTalk Forum</h1>
-                        <p className='text-xl'>Share ideas, ask questions, and connect with developers worldwide</p>
+                        <h1 className='text-3xl lg:text-5xl text-white font-bold'>Welcome to Our NodeTalk Forum</h1>
+                        <p className='text-xl text-white'>Share ideas, ask questions, and connect with developers worldwide</p>
                         <label className="input bg-white text-black w-full rounded">
                             <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                 <g
@@ -93,7 +93,7 @@ const Banner = ({ setSearch }) => {
                             </ul>
                         )}
                     </div>
-                    <Lottie className='w-60' animationData={forumanimation} loop={true} />
+                    <Lottie className='w-40 md:w-80 xl:w-88 xl:pl-20' animationData={forumanimation} loop={true} />
                 </div>
             </div>
         </div>
